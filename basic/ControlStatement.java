1a999f7864168a141cf07b76b2181d990d3d9d98
rajendra-chimala <rajendrachimala5@gmail.com>
2025-03-29 19:42:04 +0545

    first commit

---
+++ basic/ControlStatement.java
@@ -0,0 +1,15 @@
+
+public class ControlStatement {
+
+    public static void main(String[] args) {
+        int age = 10;
+        if (age < 18) {
+            System.out.println("You are a minor");
+        } else if (age == 18) {
+            System.out.println("You are a teenager");
+        } else {
+            System.out.println("You are an adult");
+        }
+    }
+
+}
