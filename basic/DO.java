d6dd69fcd967b07a67d51fbdd97bb292d5aea34d
rajendra-chimala <rajendrachimala5@gmail.com>
2025-03-29 19:55:30 +0545

    first commit

---
+++ basic/DO.java
@@ -0,0 +1,21 @@
+
+public class DO {
+
+    public static void main(String[] args) {
+        //Variables 
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Addition : " + a + b);
+        System.out.println("Multiplication : " + a * b);
+        System.out.println("Division : " + (float) a / b);
+        System.out.println("Subtraction : " + (b - a));
+        System.out.println("Modulus : " + a % b);
+        System.out.println("Increment : " + ++a);
+        System.out.println("Decrement : " + --a);
+        System.out.println("Square : " + a * a);
+        System.out.println("Cube : " + a * a * a);
+
+    }
+
+}
---
+++ basic/Loops.java
@@ -0,0 +1,31 @@
+
+public class Loops {
+
+    public static void main(String[] a) {
+
+        //For loop 
+        System.out.println("For Loop Execution ...");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+        //DO while loop 
+        System.out.println("DO While Loop Execution ...");
+
+        int i = 0;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
+
+        ///While loop execution
+        System.out.println("While Loop Execution ...");
+        int j = 0;
+
+        while (j < 5) {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
